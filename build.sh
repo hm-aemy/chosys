d839a9e94cc551dd8aaa3e40657fbda319a68581
Emil J. Tywoniak <emil@tywoniak.eu>
2025-09-17 15:09:19 +0200

    Clean up build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 set -euo pipefail
-#LLVM
 
-mkdir -p ./circt/llvm/build;
+#LLVM
+mkdir -p ./circt/llvm/build
 cmake -S ./circt/llvm/llvm -B ./circt/llvm/build -G Ninja \
   -DLLVM_ENABLE_PROJECTS="mlir"  \
   -DLLVM_TARGETS_TO_BUILD="host"   \
@@ -11,15 +11,15 @@ cmake -S ./circt/llvm/llvm -B ./circt/llvm/build -G Ninja \
   -DLLVM_ENABLE_LLD=ON \
   -DLLVM_PARALLEL_LINK_JOBS=1 \
   -DLLVM_OPTIMIZED_TABLEGEN=ON \
-  -DLLVM_CCACHE_BUILD=ON;
-ninja -C ./circt/llvm/build;
+  -DLLVM_CCACHE_BUILD=ON
+ninja -C ./circt/llvm/build
 
 #CIRCT
-mkdir -p ./circt/build;
-cmake -S ./circt -B ./circt/build --preset debug;
-ninja -C ./circt/build ;
+mkdir -p ./circt/build
+cmake -S ./circt -B ./circt/build --preset debug
+ninja -C ./circt/build
 
 #rtlil-emit
-mkdir -p ./rtlil-emit/build;
-cmake -S ./rtlil-emit -B ./rtlil-emit/build --preset debug;
+mkdir -p ./rtlil-emit/build
+cmake -S ./rtlil-emit -B ./rtlil-emit/build --preset debug
 ninja -C ./rtlil-emit/build;
